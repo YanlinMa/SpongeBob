61df885c246db3ba974ca1964cd99c30e2566b1e
hyketwig <jli31@stuy.edu>
2016-01-18 12:44:48 -0500

    Player v2

--- Player.java
+++ Player.java
@@ -1,10 +1,35 @@
-public class Player extends Piece { //is this notation right?
+/*THINGS TO DO:
 
+  - actually make it look like a piece
+
+/*TO THINK ABOUT / DO:
+  
+  - need default constructor?
+
+ */
+
+public class Player extends Piece { 
+
+    
+    // overloaded constructor
+    public Player(boolean s) {
+	king = false;
+	status = s;
+    }
 
     public String toString() {
-    }
-    
+	String retStr = "";
+	
+	if (!getStatus()) 
+	    retStr = "=====\n=====\n=====";
+
+	else if (isKing())
+	    retStr = "=====\nPLkng\n=====";
+
+	else
+	    retStr = "=====\nPLpce\n=====";
+
+	return retStr;
+    }      
 
 }
-
-
