23055d15155aaf42d17c7a24af92fea7c0fd6fa6
YanlinMa <yma3@stuy.edu>
2016-01-23 20:29:05 -0500

    Update LevelH.java

--- LevelH.java
+++ LevelH.java
@@ -1,1 +1,5 @@
-L
+//Hard
+//don't move the last row of chess pieces unless absolutely necessary
+//other pieces move to form a pyramid
+//get as many pieces to the opposite side
+//should be based on user moves, to kill off user pieces and prevent user from reaching opponent side
