92ba888a809044d41002ed2da80aa3925d75cf34
YanlinMa <yma3@stuy.edu>
2016-01-24 22:07:34 -0500

    Update Empty.java

--- Empty.java
+++ Empty.java
@@ -1,10 +1,3 @@
-/* THINGS TO THINK ABOUT / DO:
-
- - does this have to be a Piece (well, no, but should it?)
-(currently is one bc don't want Object[][] but that's arbitrary)
-
-*/
-
 public class Empty extends Piece { 
 
     public String toString() {
