d0da33b3e0cd8242cb48ee01cbe0f36e09a3f17d
YanlinMa <yma3@stuy.edu>
2016-01-25 02:34:07 -0500

    Update LevelE.java

--- LevelE.java
+++ LevelE.java
@@ -1,17 +1,19 @@
 import java.util.ArrayList;
 
+//level easy
+
 public class LevelE extends Board {
     
-    //move random opponent piece randomly, no kings
-    //assumes there are moves left
+    //move random opponent piece in random direction
     public String[] AIMove() {
+    	
 	String[] ret = new String[2];
 
         //select random piece from opponents
 	Player x = opponents.get((int)(Math.random()*opponents.size()));
 	ret[0] = x.getID();
 	
-	//select random move from that player's moves list
+	//select random move from that piece's moves list
         String m = x.randMove();
 	ret[1] = m;
 
