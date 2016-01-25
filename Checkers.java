e148e6548795f8061b041c6ff6f6f4bb4e88c9dc
YanlinMa <yma3@stuy.edu>
2016-01-24 23:18:30 -0500

    Update Checkers.java

--- Checkers.java
+++ Checkers.java
@@ -84,6 +84,7 @@ public class Checkers {
 	    //move users piece -- STILL NEED EXTRA JUMP FXN
 	    b.move(r1,c1,r2,c2);
 	    System.out.println();
+	    System.out.println("Computer's turn.");
 	    System.out.print(b);
 
 	    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
